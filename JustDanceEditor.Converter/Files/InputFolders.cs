e3c162ca208d172873315a7de2a327bc5d8f3214
MrKev312 <34964788+MrKev312@users.noreply.github.com>
2024-09-03 12:28:44 +0200

    Add FileSystem class and related folder management classes
    Introduced a new `FileSystem` class in the `JustDanceEditor.Converter.Files` namespace to handle various file system operations related to the conversion process. Added a constructor and properties to manage song name, platform type, and folder objects. Implemented methods to initialize song ID and platform type, retrieve paths, and read files without null characters.
    
    Added `InputFolders`, `OutputFolders`, `TempFolders`, and `TemplateFiles` classes to manage respective folder paths and operations. Included a method in `OutputFolders` to initialize cache numbers based on existing cache folders and their sizes.

---
+++ JustDanceEditor.Converter/Files/InputFolders.cs
@@ -0,0 +1,25 @@
+﻿namespace JustDanceEditor.Converter.Files;
+
+public class InputFolders
+{
+    public InputFolders(FileSystem fileSystem)
+    {
+        this.fileSystem = fileSystem;
+    }
+
+    readonly FileSystem fileSystem;
+    public string InputFolder => fileSystem.ConversionRequest.InputPath;
+
+
+    public string MapCacheFolder => Path.Combine("cache", "itf_cooked", fileSystem.PlatformType, "world", "maps", fileSystem.SongName);
+    public string MapWorldFolder => Path.Combine("world", "maps", fileSystem.SongName);
+
+    public string MediaFolder => Path.Combine(MapWorldFolder, "media");
+
+    public string AudioFolder => Path.Combine(MapCacheFolder, "audio");
+    public string MenuArtFolder => Path.Combine(MapCacheFolder, "menuart", "textures");
+    public string TimelineFolder => Path.Combine(MapCacheFolder, "timeline", fileSystem.SongName);
+    public string PictosFolder => Path.Combine(TimelineFolder, "pictos");
+    // WiiU is hardcoded here, but should be a variable, NX => WiiU, rest idk
+    public string MovesFolder => Path.Combine(MapWorldFolder, "timeline", "moves", "wiiu");
+}
