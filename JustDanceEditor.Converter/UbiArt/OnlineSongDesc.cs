2e2aa909fe976f0da63089d8a4f912bd55b65671
MrKev312 <34964788+MrKev312@users.noreply.github.com>
2024-11-04 13:17:46 +0100

    Initialize default values for properties in OnlineSongDesc.cs
    The properties `artist`, `credits`, `lyricsColor`, `mapName`,
    `parentMapName`, and `title` are now initialized to empty
    strings (""). The `assets` property is initialized with a new
    instance of the `Assets` class, and the `tags` array is
    initialized to an empty array ([]).

--- JustDanceEditor.Converter/UbiArt/OnlineSongDesc.cs
+++ JustDanceEditor.Converter/UbiArt/OnlineSongDesc.cs
@@ -3,22 +3,22 @@
 // For JDUnlimited server JSON
 internal class OnlineSongDesc
 {
-    public string artist { get; set; }
-    public Assets assets { get; set; }
+    public string artist { get; set; } = "";
+    public Assets assets { get; set; } = new();
     public int coachCount { get; set; }
-    public string credits { get; set; }
+    public string credits { get; set; } = "";
     public int difficulty { get; set; }
-    public string lyricsColor { get; set; }
+    public string lyricsColor { get; set; } = "";
     public int lyricsType { get; set; }
     public int mainCoach { get; set; }
     public float mapLength { get; set; }
-    public string mapName { get; set; }
+    public string mapName { get; set; } = "";
     public int originalJDVersion { get; set; }
-    public string parentMapName { get; set; }
+    public string parentMapName { get; set; } = "";
     public int status { get; set; }
     public int sweatDifficulty { get; set; }
-    public string[] tags { get; set; }
-    public string title { get; set; }
+    public string[] tags { get; set; } = [];
+    public string title { get; set; } = "";
 
     // Allow conversion from OnlineSongDesc to SongDesc
     public static explicit operator SongDesc(OnlineSongDesc onlineSongDesc)
