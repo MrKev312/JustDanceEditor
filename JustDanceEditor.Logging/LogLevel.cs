1b675083726ea3905672e0872fb8565d76ffd53c
MrKev312 <34964788+MrKev312@users.noreply.github.com>
2026-01-04 19:41:07 +0100

    Fully remove old logging stuff

--- JustDanceEditor.Logging/LogLevel.cs
+++
@@ -1,8 +0,0 @@
-// Removed enum. Use Microsoft.Extensions.Logging.LogLevel instead.
-namespace JustDanceEditor.Logging;
-
-[System.Obsolete("Use Microsoft.Extensions.Logging.LogLevel instead of JustDanceEditor.Logging.LogLevel.")]
-public enum LogLevel
-{
-    // Intentionally left empty to trigger compile-time changes where necessary.
-}
--- JustDanceEditor.Logging/Logger.cs
+++
@@ -1,7 +0,0 @@
-namespace JustDanceEditor.Logging;
-
-[System.Obsolete("JustDanceEditor.Logging.Logger has been removed. Use Microsoft.Extensions.Logging.ILogger instead.")]
-public static class Logger
-{
-    public static void Log(string message, object? _ = null) => throw new NotSupportedException("Legacy Logger removed. Use Microsoft.Extensions.Logging.ILogger.");
-}
